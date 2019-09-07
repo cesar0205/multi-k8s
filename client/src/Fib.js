14abc2f68c5a9b2d3afc30ebb98c842a1df841b6
Cesar <cesar_ipn2@hotmail.com>
2019-09-07 15:28:08 -0500

    added memory allocation 19

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -10,8 +10,10 @@ class Fib extends Component {
   };
 
   componentDidMount() {
-    this.fetchValues();
-    this.fetchIndexes();
+    setInterval(() => {
+      this.fetchValues();
+      this.fetchIndexes();
+    }, 1000)
   }
 
   async fetchValues() {
